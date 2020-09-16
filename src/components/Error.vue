ed955fb6efae09d66272a35f55601ccf837b13f6
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2020-09-16 01:06:38 +0100

    Created Error and Success Page

---
+++ src/components/Error.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+      <v-col>
+        <h1 class="display-2 font-weight-bold mb-3" style="color:red;">Error</h1>
+
+        <p class="subheading font-weight-regular">
+          Something went wrong
+        </p>
+        <br>Send a message to 08139349336 with the following details refKey: <b>{{refKey}}</b>
+        and transaction id: <b>{{transId}}</b>
+      </v-col>
+
+      <v-col cols="12">
+        <h2 class="headline font-weight-bold mb-3">If you have any question, contact:</h2>
+        <ul>
+          <li>Nafi'u Zugachi (President): +2348012345678</li>
+          <li>Ibrahim Abdullahi (DPS): +2348139349336</li>
+          <li>Imran (PRO): +2348139349936</li>
+        </ul>
+      </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      transId: localStorage.getItem('transId'),
+      refKey:localStorage.getItem('refKey')
+    }
+  }
+}
+</script>
---
+++ src/components/Success.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+      <v-col>
+        <h1 class="display-2 font-weight-bold mb-3">Success</h1>
+
+        <p class="subheading font-weight-regular">
+          You've successfully submitted your details, do you want to submit another?
+        </p>
+      </v-col>
+
+      <v-col class cols="12">
+        <v-btn color="primary" to="/payment">Submit Another</v-btn>
+      </v-col>
+
+      <v-col cols="12">
+        <h2 class="headline font-weight-bold mb-3">If you have any question, contact:</h2>
+        <ul>
+          <li>Nafi'u Zugachi (President): +2348012345678</li>
+          <li>Ibrahim Abdullahi (DPS): +2348139349336</li>
+          <li>Imran (PRO): +2348139349936</li>
+        </ul>
+      </v-col>
+  </v-container>
+</template>
